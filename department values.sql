14fc6867a02986c6b97bd14950e074d979636db2
kazzyl <165019528+kazzyl@users.noreply.github.com>
2024-03-26 12:05:14 +0100

    Add files via upload

---
+++ department values.sql
@@ -0,0 +1,15 @@
+SELECT * FROM hr_management_sys.department;
+
+insert into hr_management_sys.department values
+(1, 'hr', '2019-04-23'),
+(2, 'finance','2020-03-17'),
+(3, 'IT', '2019-02-30'),
+(4, 'Sales','2019-01-23'),
+(5, 'customer_service','2021-03-12'),
+(6,'operation','2020-03-30'),
+(10,'adim','2022-06-15'),
+(11, 'finance','2023-10-25'),
+(12,'operation','2020-07-15'),
+(13,'customer_service','2021-05-20'),
+(14,'hr','2022-08-24'),
+(15, 'sales','2021-09-18');
